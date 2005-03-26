de5801a50610aec8bc06e6dd6ec4d44607b6cfba
Gregory John Casamento <greg.casamento@gmail.com>
2005-03-26 19:45:04 +0000

    Moving preferences files into GormPrefs framework.
    git-svn-id: svn+ssh://svn.gna.org/svn/gnustep/apps/gorm/branches/build_reorg_branch@20977 72102866-910b-0410-8b05-ffd578937521

---
+++ GormPrefs/GormGeneralPref.h
@@ -0,0 +1,26 @@
+#ifndef INCLUDED_GormGeneralPref_h
+#define INCLUDED_GormGeneralPref_h
+
+#include <Foundation/NSObject.h>
+#include <AppKit/NSView.h>
+
+@interface GormGeneralPref : NSObject
+{
+  id window;
+  id backupButton;
+  id inspectorButton;
+  id palettesButton;
+  id archiveMatrix;
+
+  id _view;
+}
+
+- (NSView *) view;
+- (void) palettesAction: (id)sender;
+- (void) inspectorAction: (id)sender;
+- (void) backupAction: (id)sender;
+- (void) archiveAction: (id)sender;
+@end
+
+
+#endif
